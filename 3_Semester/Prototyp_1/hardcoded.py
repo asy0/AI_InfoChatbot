002e66393c024740bad578c4ae3c1b210ea48ff6
Zeliha Vural <zeliha@outlook.com>
2024-12-20 01:32:12 +0100

    hardcoded end

--- 3_Semester/Prototyp_1/hardcoded.py
+++ 3_Semester/Prototyp_1/hardcoded.py
@@ -1,11 +1,9 @@
 import spacy
 
-# Lade das spaCy-Modell
-nlp = spacy.load("de_core_news_lg")
+nlp = spacy.load("de_core_news_lg") #spacy Modell
 
 
 def analyze_text_hardcoded():
-    # Hardcodierter Text
     text = """
     Es war einmal ein Mädchen, dem war die Mutter gestorben. Ihre neue Stiefmutter hatte zwei Töchter mit ins Haus gebracht und sie waren alle sehr gemein zu ihr. Sie musste den ganzen Tag schwer arbeiten, früh aufstehen, Wasser tragen, Feuer machen, kochen und waschen. Abends, wenn sie müde war, musste sie sich neben den Herd in die Asche legen. Und weil sie darum immer staubig und schmutzig war, nannten sie es Aschenputtel.
     Es begab sich, daß der König ein großes Fest veranstaltete, auf dem sich der Prinz eine Gemahlin aussuchen sollte. Aschenputtel bat die Stiefmutter, sie möchte ihm erlauben hinzugehen. „Aschenputtel,“ antwortete die Stiefmutter, „du bist voll Staub und Schmutz und willst zum Fest? Du darfst nicht mit, denn du hast keine prächtigen Kleider.“ Darauf eilte sie mit ihren stolzen Töchtern fort.
@@ -15,18 +13,14 @@ def analyze_text_hardcoded():
     Und sie lebten glücklich alle Tage.
     """
 
-    #Text bereinigen
     cleaned_text = " ".join(text.split())
     doc = nlp(cleaned_text)
     # Entitäten extrahieren
     entities = [(ent.text, ent.label_) for ent in doc.ents]
 
-    sentences = [sent.text.strip() for sent in doc.sents]
-    unique_entities = list(set(entities))
-
     return {
-        "entities": unique_entities,
-        "sentences": sentences
+        "entities": list(set(entities)),
+        "sentences": [sent.text.strip() for sent in doc.sents]
     }
 
 if __name__ == "__main__":
